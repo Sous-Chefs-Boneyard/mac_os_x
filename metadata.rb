65da303f26d78fdc668ddb2e8ae8314815f3244c
Julian C. Dunn <jdunn@getchef.com>
2014-03-03 18:52:40 -0500

    Bump devodd

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "cookbooks@housepub.org"
 license          "Apache 2.0"
 description      "Manage OS X user defaults settings"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.4.4"
+version          "1.4.5"
 supports         "mac_os_x"
