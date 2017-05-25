d60e4c0ce0e1ada08f4e1a5724872759a848c431
Meg Cassidy <kameghamegha@users.noreply.github.com>
2017-05-25 08:20:43 -0700

    Added resource into default recipe for testing purposes

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,3 +18,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+mac_os_x_userdefaults 'Disable fast user switching' do
+  domain '/Library/Preferences/.GlobalPreferences'
+  key 'MultipleSessionEnabled'
+  value 0
+end
