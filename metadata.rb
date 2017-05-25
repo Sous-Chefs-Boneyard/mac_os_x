02a6af7b8497eff84dfd16714f26c9f90f3420f5
Meg Cassidy <kameghamegha@users.noreply.github.com>
2017-05-25 13:29:04 -0700

    Updated maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'mac_os_x'
 source_url       'https://github.com/sous-chefs/mac_os_x' if respond_to?(:source_url)
 issues_url       'https://github.com/sous-chefs/mac_os_x/issues' if respond_to?(:issues_url)
-maintainer       'Meg Cassidy'
-maintainer_email 'Cassidy.Meg.L@gmail.com'
+maintainer       'Sous Chefs'
+maintainer_email 'help@sous-chefs.org'
 license          'Apache 2.0'
 description      'Manage OS X user defaults settings and plist files'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
