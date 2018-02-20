d25d4b3b04cfd03e6433dc1c1864b1ef9e543308
Tim Smith <tsmith@chef.io>
2018-02-20 14:39:24 -0800

    Update test kitchen config
    This still requires the user provide their own box, but this gets them closer
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/test/recipes/userdefaults.rb
+++ test/fixtures/cookbooks/test/recipes/userdefaults.rb
@@ -3,3 +3,31 @@ mac_os_x_userdefaults 'Disable fast user switching' do
   key 'MultipleSessionEnabled'
   value 0
 end
+
+macos_userdefaults 'finder expanded save dialogs' do
+  domain 'NSNavPanelExpandedStateForSaveMode'
+  global true
+  value 'TRUE'
+  type 'bool'
+end
+
+macos_userdefaults 'enable macOS firewall' do
+  domain '/Library/Preferences/com.apple.alf'
+  key 'globalstate'
+  value '1'
+  type 'int'
+end
+
+macos_userdefaults 'set dock size' do
+  domain 'com.apple.dock'
+  type 'integer'
+  key 'tilesize'
+  value '20'
+end
+
+macos_userdefaults 'disable time machine normal schedule' do
+  domain '/System/Library/LaunchDaemons/com.apple.backupd-auto'
+  key 'Disabled'
+  value '1'
+  sudo true
+end
--- test/integration/userdefaults/example_spec.rb
+++ test/integration/userdefaults/example_spec.rb
@@ -1,7 +1,3 @@
-# # encoding: utf-8
-
-# Inspec test for recipe mac_os_x::test
-
 describe command('defaults read /Library/Preferences/.GlobalPreferences "MultipleSessionEnabled"') do
   its('stdout') { should match(/0/) }
 end
